4c9a2edfe71e60169cdcca8e8b8f20e13d5909a0
Giacomo Barbieri <jaack@routescan.io>
2026-02-27 12:01:14 +0100

    fix: remove .next-output from git, revert to API-only vercel.json

--- .next-output/static/chunks/app/layout-d94390fce1a8bfca.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{6745:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,3736,23))},3736:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[585,770,229,358],()=>s(6745)),_N_E=e.O()}]);
--- .next-output/static/chunks/app/page-342a913006bb74df.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[190,974],{5389:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,6443,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[443,770,229,358],()=>s(5389)),_N_E=e.O()}]);
